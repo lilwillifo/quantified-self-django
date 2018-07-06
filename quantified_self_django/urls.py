e422cfaad7bec19922ae2c3eb17dc6fbeaf83286
Margaret Williford <margaretwilliford4@gmail.com>
2018-07-06 10:03:09 -0600

    update

--- quantified_self_django/urls.py
+++ quantified_self_django/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls'))
+    path('api/v1/', include('api.urls')) 
 ]
