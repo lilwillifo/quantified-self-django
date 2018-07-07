6aad9251aeb4fa466e45f7b0e437490e54747075
Margaret Williford <margaretwilliford4@gmail.com>
2018-07-07 12:30:53 -0600

    testing rest of foods CRUS functionality

--- api/tests.py
+++ api/tests.py
@@ -35,3 +35,32 @@ class ViewTestCase(TestCase):
     def test_api_can_create_a_food(self):
         """Test the api has food creation capability."""
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_api_can_get_a_food(self):
+        """Test the api can get a given food."""
+        food = Food.objects.get()
+        response = self.client.get(
+          reverse('details', kwargs={'pk': food.id}),
+          format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, food)
+
+    def test_api_can_update_food(self):
+        """Test the api can update a given food."""
+        food = Food.objects.get()
+        change_food = {'name': 'Something new', 'calories': '100'}
+        res = self.client.put(
+          reverse('details', kwargs={'pk': food.id}),
+          change_food, format='json'
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_api_can_delete_food(self):
+        """Test the api can delete a food."""
+        food = Food.objects.get()
+        response = self.client.delete(
+          reverse('details', kwargs={'pk': food.id}),
+          format='json',
+          follow=True)
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import CreateView
+from .views import DetailsView
 
 urlpatterns = {
     url(r'^foods', CreateView.as_view(), name="create"),
+    url(r'^foods/(?P<pk>[0-9]+)/$',
+            DetailsView.as_view(), name="details"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
